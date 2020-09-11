a62210ae9263d92e490fafa426c74450e10b8645
Illia Nechesa <nechesaillia@gmail.com>
2020-09-11 10:33:26 +0200

    chore: add dishdetail for assignment one
    Signed-off-by: Illia Nechesa <nechesaillia@gmail.com>

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 
-const DISHES: Dish[] = [
+export const DISHES: Dish[] = [
   {
     id: '0',
     name: 'Uthappizza',
